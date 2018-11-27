b1dbed59eede72a0742d41026d53f70902677f59
eihigh <eihigh.contact@gmail.com>
2018-11-27 14:58:02 +0900

    stm: add total count (experimental)

--- stm.go
+++ stm.go
@@ -2,15 +2,15 @@ package sio
 
 // Stm is a simple state keeper
 type Stm struct {
+	total int
 	count int
 	state int
-
-	phases map[string]int
 }
 
 // Update increments the count
 func (s *Stm) Update() {
 	s.count++
+	s.total++
 }
 
 // Current returns current state
@@ -21,6 +21,11 @@ func (s Stm) Current() int {
 // Elapsed returns elapsed count from changing state
 func (s Stm) Elapsed() int {
 	return s.count
+}
+
+// TotalElapsed returns total elapsed time from beginning
+func (s Stm) TotalElapsed() int {
+	return s.total
 }
 
 // HasElapsed reports the time has elapsed or not
@@ -36,6 +41,12 @@ func (s Stm) RatioTo(base int) float64 {
 // Reset resets the count
 func (s *Stm) Reset() {
 	s.count = 0
+}
+
+// ResetAll resets the count and the total
+func (s *Stm) ResetAll() {
+	s.count = 0
+	s.total = 0
 }
 
 // To changes current state
