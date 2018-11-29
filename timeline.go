89c03399d0d895e9325fc5ce28f3fde555ca6475
eihigh <eihigh.contact@gmail.com>
2018-11-29 15:22:42 -0800

    add timeline

--- timeline.go
+++ timeline.go
@@ -1,25 +1,31 @@
 package sio
 
-type Phase struct {
-	name  string
-	limit int
+type phase struct {
+	end   int
+	state int
 }
 
 type Timeline struct {
-	phases []Phase
+	phases []phase
+	state  State
 }
 
-// PushByDiff pushes a phase by difference.
-func (t *Timeline) PushByDiff(name string, diff int) {
-	total := t.phases[len(t.phases)-1].limit
-	t.phases = append(t.phases, Phase{name, total + diff})
+func (t *Timeline) AppendByDiff(state, diff int) {
+	total := 0
+	if len(t.phases) > 0 {
+		total = t.phases[len(t.phases)-1].end
+	}
+	t.phases = append(t.phases, phase{
+		end:   total + diff,
+		state: state,
+	})
 }
 
-func (t Timeline) Phase(count int) string {
+func (t *Timeline) Current() int {
 	for _, p := range t.phases {
-		if count < p.limit {
-			return p.name
+		if t.state.Count() < p.end {
+			return p.state
 		}
 	}
-	return ""
+	return 0
 }
