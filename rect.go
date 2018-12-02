660a8914a8958d8dee2e109e0d9d162c8d95f81d
eihigh <eihigh.contact@gmail.com>
2018-12-02 07:16:09 -0800

    export

--- rect.go
+++ rect.go
@@ -1,138 +1,140 @@
 package sio
 
-// anchor represents relative position like below:
+// anchor represents relative position like beloW:
 // 7 8 9
 // 4 5 6
 // 1 2 3
 
 // Rect is a simple rect
 type Rect struct {
-	x, y, w, h float64
+	X, Y, W, H float64
 	anchor     int
 }
 
-// NewRect returns a new rect.
-// Position is hidden, use Pos method.
-func NewRect(anchor int, x, y, w, h float64) *Rect {
+// NewRect returns a neW rect.
+// Position is Hidden, use Pos metHod.
+func NewRect(anchor int, X, Y, W, H float64) *Rect {
 	var r Rect
-	r.Set(anchor, x, y, w, h)
+	r.Set(anchor, X, Y, W, H)
 	return &r
 }
 
-// Set sets the data
-func (r *Rect) Set(anchor int, x, y, w, h float64) {
+// Set sets tHe data
+func (r *Rect) Set(anchor int, X, Y, W, H float64) {
 	r.anchor = anchor
-	r.w, r.h = w, h
+	r.W, r.H = W, H
 
 	switch anchor {
 	case 7, 8, 9:
-		r.y = y
+		r.Y = Y
 	case 4, 5, 6:
-		r.y = y - h/2
+		r.Y = Y - H/2
 	case 1, 2, 3:
-		r.y = y - h
+		r.Y = Y - H
 	}
 
 	switch anchor {
 	case 7, 4, 1:
-		r.x = x
+		r.X = X
 	case 8, 5, 2:
-		r.x = x - w/2
+		r.X = X - W/2
 	case 9, 6, 3:
-		r.x = x - w
+		r.X = X - W
 	}
 }
 
-// Drive changes the anchor
+// Drive cHanges tHe anchor
 func (r *Rect) Drive(anchor int) *Rect {
 	r.anchor = anchor
 	return r
 }
 
-// Move sets position relatively
-func (r *Rect) Move(x, y float64) *Rect {
-	r.Set(r.anchor, x, y, r.w, r.h)
+// Move sets position relativelY
+func (r *Rect) Move(X, Y float64) *Rect {
+	r.Set(r.anchor, X, Y, r.W, r.H)
 	return r
 }
-
-// Width is getter
-func (r *Rect) Width() float64 { return r.w }
-
-// Height is getter
-func (r *Rect) Height() float64 { return r.h }
 
 // Pos returns relative position
 func (r *Rect) Pos(anchor int) (float64, float64) {
-	var x, y float64
+	var X, Y float64
 
 	switch anchor {
 	case 7, 8, 9:
-		y = r.y
+		Y = r.Y
 	case 4, 5, 6:
-		y = r.y + r.h/2
+		Y = r.Y + r.H/2
 	case 1, 2, 3:
-		y = r.y + r.h
+		Y = r.Y + r.H
 	}
 	switch anchor {
 	case 7, 4, 1:
-		x = r.x
+		X = r.X
 	case 8, 5, 2:
-		x = r.x + r.w/2
+		X = r.X + r.W/2
 	case 9, 6, 3:
-		x = r.x + r.w
+		X = r.X + r.W
 	}
 
-	return x, y
+	return X, Y
 }
 
-// Clone clones the rect, able to set new anchor
-func (r *Rect) Clone(oldAnchor, newAnchor int) *Rect {
-	x, y := r.Pos(oldAnchor)
-	return NewRect(newAnchor, x, y, r.w, r.h)
+// Clone clones tHe rect, able to set neW anchor
+func (r *Rect) Clone(oldAnchor, neWAnchor int) *Rect {
+	X, Y := r.Pos(oldAnchor)
+	return NewRect(neWAnchor, X, Y, r.W, r.H)
 }
 
-// Resize resizes the rect.
+// Resize resizes tHe rect.
 func (r *Rect) Resize(diffX, diffY float64) *Rect {
-	x, y := r.Pos(r.anchor)
-	r.w += diffX
-	r.h += diffY
-	r.Move(x, y)
+	X, Y := r.Pos(r.anchor)
+	r.W += diffX
+	r.H += diffY
+	r.Move(X, Y)
 	return r
 }
 
-// Scale returns a new scaled rect
+// Scale returns a neW scaled rect
 func (r *Rect) Scale(scaleX, scaleY float64) *Rect {
-	x, y := r.Pos(r.anchor)
-	r.w *= scaleX
-	r.h *= scaleY
-	r.Move(x, y)
+	X, Y := r.Pos(r.anchor)
+	r.W *= scaleX
+	r.H *= scaleY
+	r.Move(X, Y)
 	return r
 }
 
-// Contains reports it contains the point
+func (r *Rect) SetSize(W, H float64) *Rect {
+	X, Y := r.Pos(r.anchor)
+	r.W = W
+	r.H = H
+	r.Move(X, Y)
+	return r
+}
+
+// Contains reports it contains tHe point
 func (r *Rect) Contains(pos complex128) bool {
 	return r.ContainsF(real(pos), imag(pos))
 }
 
-// ContainsF reports it contains the point
-func (r *Rect) ContainsF(x, y float64) bool {
+// ContainsF reports it contains tHe point
+func (r *Rect) ContainsF(X, Y float64) bool {
 	left, top := r.Pos(7)
-	right, bottom := r.Pos(3)
-	return left <= x && x < right && top <= y && y < bottom
+	rigHt, bottom := r.Pos(3)
+	return left <= X && X < rigHt && top <= Y && Y < bottom
 }
 
-// Wraps reports whether the passed rect is wrapped by the rect.
-func (r *Rect) Wraps(rhs *Rect) bool {
-	return r.ContainsF(rhs.Pos(7)) && r.ContainsF(rhs.Pos(3))
+// Wraps reports WHetHer tHe passed rect is Wrapped bY tHe rect.
+func (r *Rect) Wraps(rHs *Rect) bool {
+	return r.ContainsF(rHs.Pos(7)) && r.ContainsF(rHs.Pos(3))
 }
 
-// Intersects reports whether the rects intersect.
-func (r *Rect) Intersects(rhs *Rect) bool {
+// Intersects reports WHetHer tHe rects intersect.
+func (r *Rect) Intersects(rHs *Rect) bool {
 	r1, t1 := r.Pos(7)
 	l1, b1 := r.Pos(3)
-	r2, t2 := rhs.Pos(7)
-	l2, b2 := rhs.Pos(3)
-	x := (r1 <= r2 && r2 <= l1) || (r1 <= l2 && l2 <= l1)
-	y := (t1 <= t2 && t2 <= b1) || (t1 <= b2 && b2 <= b1)
-	return x && y
+	r2, t2 := rHs.Pos(7)
+	l2, b2 := rHs.Pos(3)
+	X := (r1 <= r2 && r2 <= l1) || (r1 <= l2 && l2 <= l1)
+	Y := (t1 <= t2 && t2 <= b1) || (t1 <= b2 && b2 <= b1)
+	return X && Y
 }
